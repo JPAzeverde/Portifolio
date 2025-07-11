c6c3ee3fc5725590b670841bb5fb94c0bd0a05c6
JOAO PEDRO BRITO DE AZEVEDO <joaopzevedo@unilavras.edu.br>
2025-07-11 10:58:02 -0300

    Melhorando a página home

--- src/components/PortifolioSection.tsx
+++ src/components/PortifolioSection.tsx
@@ -1,25 +1,35 @@
-import type { ReactNode } from "react";
-import { Box, Container } from "@mui/material";
+import { Box, Container, Grid } from "@mui/material";
 import { useLanguage } from "@/context";
 import { StyledH3, StyledP } from "./Typographies";
+import { pxToRem } from "@/utils";
 
-type Props = {
-  children?: ReactNode;
-};
 
-export function PortifolioSection({ children }: Props) {
+export function PortifolioSection() {
   const { translations } = useLanguage();
   return (
     <Box className="bgSecundary" sx={{ py: 6 }}>
       <Container maxWidth="lg">
         <StyledH3 className="mb-2">{translations.about.title}</StyledH3>
-        <StyledP
-          className="mb-2 fz-18"
-          dangerouslySetInnerHTML={{
-            __html: translations.about.description1,
-          }}
-        />
-        {children}
+        <Grid container spacing={4}>
+          <Grid size={{ xs: 12, sm: 12, md: 5, lg: 5 }} sx={{ display: 'flex', height: '100%', minHeight: '300px', justifyContent: { xs: 'center', sm: 'center', md: 'flex-start' }, alignItems: { xs: 'center', sm: 'center', md: 'center' }, }}>
+            <img
+              src="IMG_Perfil_Portifolio.jpg"
+              alt="Foto João Pedro"
+              style={{ width: '90%', objectFit: 'cover', borderRadius: pxToRem(8) }}
+            />
+          </Grid>
+
+          <Grid size={{ xs: 12, sm: 12, md: 7, lg: 7 }}>
+            <StyledP className="mb-1">{translations.about.description1}</StyledP>
+            <StyledP className="mb-1">{translations.about.description2}</StyledP>
+            <StyledP className="mb-1">{translations.about.description3}</StyledP>
+            <StyledP className="mb-2">{translations.about.description4}</StyledP>
+          </Grid>
+
+        </Grid>
+        
+        
+
       </Container>
     </Box>
   );
